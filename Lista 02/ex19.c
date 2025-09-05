6c5bcd66454c9f675b88abbde6f6f565c1f59100
Renato Bressan <renatoikeda98@gmail.com>
2025-09-05 14:52:26 -0300

    Add files via upload

---
+++ Lista 02/ex19.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int cc, dr, rt;
+    scanf("%d", &cc);
+    scanf("%d", &dr);
+    scanf("%d", &rt);
+    if (cc < 7 && dr > 50 && rt > 80000) {
+        printf("ACO DE GRAU = 10\n");
+    } else if (cc < 7 && dr > 50 && rt <= 80000) {
+        printf("ACO DE GRAU = 9\n");
+    } else if (cc < 7 && dr <= 50 && rt <= 80000) {
+        printf("ACO DE GRAU = 8\n");
+    } else {
+        printf("ACO DE GRAU = 7\n");
+    }
+    return 0;
+}
