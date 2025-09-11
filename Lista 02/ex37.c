0f1fdcdaa69e46587151e87bee17e3de7395596b
Renato Bressan <renatoikeda98@gmail.com>
2025-09-11 17:30:28 -0300

    Add files via upload

---
+++ Lista 02/ex37.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    int checkup = 0;
+    scanf("%d", &n);
+    while (n <= 1) {
+        printf("Fatoracao nao e possivel para o numero %d!\n", n);
+        scanf("%d", &n);
+    }
+    printf("%d = ", n);
+    int i;
+    for (i = 2; i <= n; i++) {
+        if (n % i == 0) {
+            if (checkup == 0) {
+                printf("%d", i);
+                n /= i;
+                i--;
+                checkup = 1;
+            } else {
+                printf(" x %d", i);
+                n /= i;
+                i--;
+                checkup = 1;
+            }
+        }
+    }
+    printf("\n");
+    return 0;
+}
