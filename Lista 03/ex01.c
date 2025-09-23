5c8abc47c0b1519012197468b2053ba0c1c808bc
Renato Bressan <renatoikeda98@gmail.com>
2025-09-23 14:00:12 -0300

    Add files via upload

---
+++ Lista 03/ex01.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#define n 100000
+
+int main() {
+    int t;
+    int v[n];
+    int m;
+    int k;
+    scanf("%d", &t);
+    if (t < 1 || t > 100000) {
+        return 0;
+    }
+    int i;
+    for (i = 0; i < t; i++) {
+        scanf("%d", &v[i]);
+    }
+    scanf("%d", &m);
+    if (m < 1 || m > 1000) {
+        return 0;
+    }
+    while (m--) {
+        scanf("%d", &k);
+        for (i = 0; i < t; i++) {
+            if (k == v[i]) {
+                printf("ACHEI\n");
+                break;
+            }
+        }
+        if (i == t) {
+            printf("NAO ACHEI\n");
+        }
+    }
+    return 0;
+}
