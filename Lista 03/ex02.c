0db7691d0513243c5a2f62886516353965743bd8
Renato Bressan <renatoikeda98@gmail.com>
2025-09-23 14:17:23 -0300

    Add files via upload

---
+++ Lista 03/ex02.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#define n 1000
+
+int main() {
+    int t;
+    int v[n];
+    int k;
+    int dif = 0;
+    scanf("%d", &t);
+    if (t <= 0 || t > n) {
+        return 0;
+    }
+    int i;
+    for (i = 0; i < t; i++) {
+        scanf("%d", &v[i]);
+    }
+    scanf("%d", &k);
+    for (i = 0; i < t; i++) {
+        if (v[i] >= k) {
+            dif++;
+        }
+    }
+    printf("%d\n", dif);
+    return 0;
+}
