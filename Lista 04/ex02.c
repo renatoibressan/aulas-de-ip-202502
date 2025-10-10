c9faae25000b92ef4a3e104030d485c157d2e51a
Renato Bressan <renatoikeda98@gmail.com>
2025-10-10 17:30:26 -0300

    Add files via upload

---
+++ Lista 04/ex02.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int fibonacci(int t1, int t2, int n) {
+    int auxiliar, i;
+    for (i = 3; i <= n; i++) {
+        auxiliar = t1 + t2;
+        t1 = t2;
+        t2 = auxiliar;
+    }
+    return auxiliar;
+}
+
+int main() {
+    int t1, t2, n;
+    scanf("%d %d %d", &t1, &t2, &n);
+    printf("%d\n", fibonacci(t1, t2, n));
+    return 0;
+}
---
+++ Lista 04/ex06.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+double absoluto(double n) {
+    if (n < 0) return -n;
+    else return n;
+}
+
+double raiz(double n, double p) {
+    double raiz, aprox = 1, err;
+    raiz = (aprox + n / aprox) / 2;
+    aprox = raiz;
+    err = absoluto(raiz * raiz - n);
+    printf("r: %.9lf, err: %.9lf\n", raiz, err);
+    while (err > p) {
+        raiz = (aprox + n / aprox) / 2;
+        aprox = raiz;
+        err = absoluto(raiz * raiz - n);
+        printf("r: %.9lf, err: %.9lf\n", raiz, err);
+    }
+}
+
+int main() {
+    double n, e;
+    scanf("%lf %lf", &n, &e);
+    raiz(n, e);
+    return 0;
+}
