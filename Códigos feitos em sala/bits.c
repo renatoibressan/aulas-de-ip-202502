357c22bfa6b8ea3f6643e3d08ec1fbd448c55fd8
Renato Bressan <renatoikeda98@gmail.com>
2025-10-10 15:28:46 -0300

    Add files via upload

---
+++ Códigos feitos em sala/bits.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void print_byte(unsigned char const * pc) {
+    int i;
+    int w = 128;
+    unsigned char x = *pc;
+    for (i = 0; i < 8; i++) {
+        printf("%d", x / w);
+        x = x % w;
+        w = w / 2;
+    }
+}
+
+void pBytes(unsigned char * v, int n) {
+    int i;
+    for (i = 0; i < n; i++) {
+        print_byte(v + i);
+        printf(" ");
+    }
+    printf("\n");
+}
+
+int main() {
+    int x;
+    double y;
+    scanf ("%d %lf", &x, &y);
+    pBytes((unsigned char *)&x, sizeof(int));
+    pBytes((unsigned char *)&y, sizeof(double));
+}
