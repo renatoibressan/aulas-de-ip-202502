e658e44f206070af83eb891d142cb75801a3e1c0
Renato Bressan <renatoikeda98@gmail.com>
2025-10-11 11:18:49 -0300

    Add files via upload

---
+++ Lista 04/ex08.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+int next_power(int n, int p) {
+    int i = 0;
+    while (pow(i, p) < n) i++;
+    i = i - 1;
+    int x = pow(i, p);
+    int j = i + 1;
+    int y = pow(j, p);
+    if (n - x < y - n) printf("%d -> %d^%d = %d\n", n, i, p, x);
+    else printf("%d -> %d^%d = %d\n", n, j, p, y);
+}
+
+int main() {
+    int n, p;
+    scanf("%d %d", &n, &p);
+    next_power(n, p);
+    return 0;
+}
