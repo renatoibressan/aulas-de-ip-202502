c3b5005694239e0b66ba3cb33cbf603057cdc084
Renato Bressan <renatoikeda98@gmail.com>
2025-10-15 12:58:04 -0300

    Add files via upload

---
+++ Lista 03/ex13.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+void allocate(int n, char s[]) {
+    int i;
+    if (n >= strlen(s)) {
+        printf("%s\n", s);
+        return;
+    }
+    for (i = 0; i < n; i++) {
+        printf("%c", s[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int n;
+    char s[505];
+    scanf("%d", &n);
+    int i;
+    for (i = 0; i < n; i++) {
+        int m;
+        scanf("%d %[^\n]%*c", &m, s);
+        allocate(m, s);
+    }
+    return 0;
+}
---
+++ Lista 03/ex14.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int find(char A, char const * B) {
+    int i;
+    for (i = 0; B[i] != '\0'; i++) {
+        if (A == B[i]) return i;
+    }
+    if (A != B[i]) return -1;
+}
+
+int main() {
+    int n;
+    char a, b[505];
+    scanf("%d%*c", &n);
+    int i;
+    for (i = 0; i < n; i++) {
+        scanf("%c %[^\n]%*c", &a, b);
+        int valor = find(a, b);
+        if (valor == -1) printf("Caractere %c nao encontrado.\n", a);
+        else printf("Caractere %c encontrado no indice %d da string.\n", a, valor);
+    }
+    return 0;
+}
