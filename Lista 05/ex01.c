c679a7a6a63ff4b55551d9e4f110ed93c4d2da65
Renato Bressan <renatoikeda98@gmail.com>
2025-10-21 10:19:51 -0300

    Add files via upload

---
+++ Lista 05/ex01.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    double m[2][2];
+    scanf("%lf %lf %lf %lf", &m[0][0], &m[0][1], &m[1][0], &m[1][1]);
+    double d = (m[0][0] * m[1][1]) - (m[0][1] * m[1][0]);
+    printf("%.2lf\n", d);
+    return 0;
+}
