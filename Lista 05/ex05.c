3288a2ac013ba59d25fdaf1b564ec78e84f6f22a
Renato Bressan <renatoikeda98@gmail.com>
2025-10-22 20:24:45 -0300

    Add files via upload

---
+++ Lista 05/ex05.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int m[6][6];
+    int l, c;
+    for (l = 0; l < 6; l++) {
+        for (c = 0; c < 6; c++) {
+            scanf("%d", &m[l][c]);
+        }
+    }
+    int soma = 0;
+    int maior = -64;
+    for (l = 0; l + 2 < 6; l++) {
+        for (c = 0; c + 2 < 6; c++) {
+            soma = m[l][c] + m[l][c + 1] + m[l][c + 2] + m[l + 1][c + 1] + m[l + 2][c] + m[l + 2][c + 1] + m[l + 2][c + 2];
+            if (soma > maior) maior = soma;
+        }
+    }
+    printf("%d\n", maior);
+    return 0;
+}
