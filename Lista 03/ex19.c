a727f85e15a12257d7ac1d01f7225b13066e2c3f
Renato Bressan <renatoikeda98@gmail.com>
2025-10-23 09:02:26 -0300

    Add files via upload

---
+++ Lista 03/ex19.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+int diferenca(char * a, char * b) {
+    if (strlen(a) != strlen(b)) return -1;
+    int i, soma = 0;
+    for (i = 0; i < strlen(a); i++) {
+        if (b[i] > a[i]) soma += b[i] - a[i];
+        else if (a[i] > b[i]) soma += b[i] - a[i] + 26;
+    }
+    return soma;
+}
+
+int main() {
+    int t;
+    scanf("%d", &t);
+    char va[10001], vb[10001];
+    int i, distancia = 0;
+    for (i = 0; i < t; i++) {
+        scanf("%s%*c %s%*c", va, vb);
+        distancia = diferenca(va, vb);
+        if (distancia == -1) break;
+        printf("%d\n", distancia);
+    }
+    return 0;
+}
