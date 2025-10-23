9d113e674fb80768565724874450fe9248328e26
Renato Bressan <renatoikeda98@gmail.com>
2025-10-23 13:06:39 -0300

    Add files via upload

---
+++ Lista 05/ex06.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#define NL 100
+#define NC 100
+
+int main() {
+    int m[NL][NC];
+    int nl, nc, k, x;
+    scanf("%d %d %d %d", &nl, &nc, &k, &x);
+    printf("P2\n");
+    printf("%d %d\n", nl, nc);
+    printf("%d\n", 255);
+    int l, c;
+    for (l = 0; l < nc; l++) {
+        for (c = 0; c < nl; c++) {
+            m[l][c] = x;
+        }
+    }
+    for (l = k; l < nc - k; l++) {
+        for (c = k; c < nl - k; c++) {
+            m[l][c] = 0;
+        }
+    }
+    for (l = 0; l < nc; l++) {
+        for (c = 0; c < nl; c++) {
+            printf("%d ", m[l][c]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
