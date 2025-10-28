6f595bafba1e3cb7c724c0849f9ad964af4c1659
Renato Bressan <renatoikeda98@gmail.com>
2025-10-28 15:16:59 -0300

    Update ex05.c

--- Lista 04/ex05.c
+++ Lista 04/ex05.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <math.h>
-
+// Raízes de equações de grau 2
 float raizesEq2Grau(float a, float b, float c, float x1, float x2) {
     float delta = pow(b, 2) - 4 * a * c;
     if (delta > 0) {
